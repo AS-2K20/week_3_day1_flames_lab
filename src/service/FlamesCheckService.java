6e3ca81b0340eb54bd5a0db57c0687fc319293cc
AS-2K20 <amudhan.psgcas@gmail.com>
2020-07-01 22:09:41 +0530

    Flames lab with Bonus

---
+++ src/service/FlamesCheckService.java
@@ -0,0 +1,87 @@
+package service;
+
+public class FlamesCheckService{
+	public char findFlames(String name1, String name2) {
+		int i,j;
+		name1 = name1.trim();
+		name2 = name2.trim();
+		name1 = name1.toLowerCase();
+		name2 = name2.toLowerCase();
+		String name1_split[] = name1.split(" ");
+		String name2_split[] = name2.split(" ");
+		name1 = "";
+		name2 = "";
+		for(i=0; i<name1_split.length;i++) {
+			name1 = name1 + name1_split[i];
+		}
+		for(i=0; i<name2_split.length;i++) {
+			name2 = name2 + name2_split[i];			
+		}
+		int length = name1.length()+name2.length();
+		boolean name_check[] = new boolean[name2.length()];
+		for(i=0;i<name2.length();i++) {
+			name_check[i]= false;
+		}
+		for(i=0;i<name1.length();i++) {
+			for(j=0;j<name2.length();j++) {
+				if((name_check[j]==false)&&(name1.charAt(i)==name2.charAt(j))) {
+					name_check[j] = true;
+					length = length - 2;
+					break;
+				}
+			}
+		}
+		boolean flames_check[] = new boolean [6];
+		for(i=0;i<6;i++) {
+			flames_check[i] = true;
+		}
+		int count= 6;
+		int k = 1,deleted_letters=0;
+		for(j=0;j<=count;j++) {
+			if(k<=length) {
+				if(j==count) {
+					j=0;
+				}
+				if(flames_check[j]==true) {
+					k = k+1;
+				}
+			}
+			if((k-1)==length) {
+				deleted_letters = deleted_letters +1;
+				flames_check[j]=false;
+				k=1;
+			}
+		}
+		char[] a = new char[6];
+		String[] flames = new String[6];
+		String flames_ans="";
+		flames[0]="\"FRIENDS\"";
+		flames[1]="\"LOVERS\"";
+		flames[2]="\"AFFECTION\"";
+		flames[3]="\"MARRIAGE\"";
+		flames[4]="\"ENEMY\"";
+		flames[5]="\"SISTER\"";
+		a[0]='F';
+		a[1]='L';
+		a[2]='A';
+		a[3]='M';
+		a[4]='E';
+		a[5]='S';
+		char ans='a';
+		for(i=0;i<count;i++) {
+			if (flames_check[i]==true) {
+				 ans = a[i];
+				 flames_ans= flames[i];
+			}
+		}
+		System.out.println("Your name is "+name1+"."+" Your partner name is "+name2+"."+" You two are "+flames_ans);
+		return ans;
+	}
+}
+// Create a class called FlamesCheckService
+// FlamesCheckService has a method findFlames to find the flames between two names
+// char findFlames(String name1, String name2) takes two strings as arguments
+// Your task is to calculate the flames value and return the corresponding character as output 
+// You must return only the following values ['f','l','a','m','e','s']
+// change the return value at the end of the method corresponding to your return value
+
